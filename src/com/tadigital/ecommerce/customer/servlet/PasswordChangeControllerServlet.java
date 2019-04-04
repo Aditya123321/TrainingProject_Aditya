bc8e0b9f15d292140bca18b48745954dd2036849
Aditya Sharma <adityasharma_sy@srmuniv.edu.in>
2019-04-05 01:40:17 +0530

    removed warnings and errors

--- src/com/tadigital/ecommerce/customer/servlet/PasswordChangeControllerServlet.java
+++ src/com/tadigital/ecommerce/customer/servlet/PasswordChangeControllerServlet.java
@@ -1,6 +1,5 @@
 package com.tadigital.ecommerce.customer.servlet;
 
-
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -20,72 +19,98 @@ import com.tadigital.ecommerce.customer.service.CustomerService;
 @WebServlet("/PasswordChange")
 public class PasswordChangeControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PasswordChangeControllerServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public PasswordChangeControllerServlet() {
+		super();
+		
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	
+		try {
+			response.getWriter().append("Served at: ").append(request.getContextPath());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
 		Customer customer = new Customer();
 		HttpSession ses = request.getSession(); // Session object required?
-		customer=(Customer)ses.getAttribute("CUSTOMERDATA");
-		int i=customer.getId();
-		System.out.println("ID in servelet"+i);
+		
+		String setLink="CustomerAccount.jsp";
+		
+		customer = (Customer) ses.getAttribute("CUSTOMERDATA");
+		int i = customer.getId();
+		
+		
 		String oldPasswordForm = request.getParameter("f1");
 		String newPasswordForm = request.getParameter("f2");
 		String newpasswordRetypeForm = request.getParameter("f3");
-		String currPassword =  customer.getPwd(); 
+		
+		String currPassword = customer.getPwd();
 		customer.setId(i);
-		//System.out.println(oldPasswordForm + newpasswordRetypeForm+ newPasswordForm+ currPassword);
-		if( currPassword.equals(oldPasswordForm) && newPasswordForm.equals(newpasswordRetypeForm))
-		{
-			//System.out.println("hello");
+		
+		if (currPassword.equals(oldPasswordForm) && newPasswordForm.equals(newpasswordRetypeForm)) {
+		
 			customer.setPwd(newPasswordForm);
 			CustomerService customerService = new CustomerService();
-			boolean status=customerService.updatePassword(customer);
+			boolean status = customerService.updatePassword(customer);
+
 			
-			
-			System.out.println(""+status);
-			if(status) {
+			if (status) {
 				ses.setAttribute("check3", "3");
-				System.out.println("Password Updated Succesfully!");
-				RequestDispatcher rd = request.getRequestDispatcher("CustomerAccount.jsp");
-				rd.forward(request, response);
+				
+				RequestDispatcher rd = request.getRequestDispatcher(setLink);
+				try {
+					rd.forward(request, response);
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
 			} else {
 				ses.setAttribute("check3", "-3");
-				System.out.println("Error Updating the Password!!");
-				RequestDispatcher rd = request.getRequestDispatcher("CustomerAccount.jsp");
+				
+				RequestDispatcher rd = request.getRequestDispatcher(setLink);
+				try {
+					rd.forward(request, response);
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+		} else {
+			ses.setAttribute("check3", "-3");
+			
+			RequestDispatcher rd = request.getRequestDispatcher(setLink);
+			try {
 				rd.forward(request, response);
+			}catch(Exception e) {
+				e.printStackTrace();
 			}
 			
 		}
-		else
-		{
-			ses.setAttribute("check3", "-3");
-			System.out.println("Error Updating the Password!!");
-			RequestDispatcher rd = request.getRequestDispatcher("CustomerAccount.jsp");
-			rd.forward(request, response);
-			System.out.println("Passwords donot match or wrong old password!");
+		try {
+			doGet(request, response);
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
-		doGet(request, response);
+		
 	}
 
 }
-
-
