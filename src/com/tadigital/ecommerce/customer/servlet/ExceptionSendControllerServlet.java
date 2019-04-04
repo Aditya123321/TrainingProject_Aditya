8f6d5da109c8d6922b0eaf9f65c038e5fe0494a4
Aditya Sharma <adityasharma_sy@srmuniv.edu.in>
2019-04-05 01:38:49 +0530

    removed warnings and errors

--- src/com/tadigital/ecommerce/customer/servlet/ExceptionSendControllerServlet.java
+++ src/com/tadigital/ecommerce/customer/servlet/ExceptionSendControllerServlet.java
@@ -8,10 +8,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.catalina.servlet4preview.RequestDispatcher;
+
 
 import com.tadigital.ecommerce.customer.entity.Customer;
-import com.tadigital.ecommerce.customer.service.*;
+import com.tadigital.ecommerce.customer.service.CustomerService;
 
 /**
  * Servlet implementation class ExceptionSendControllerServlet
@@ -25,41 +25,65 @@ public class ExceptionSendControllerServlet extends HttpServlet {
      */
     public ExceptionSendControllerServlet() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		try {
+			response.getWriter().append("Served at: ").append(request.getContextPath());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+    @Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		Customer customer=new Customer();
 		CustomerService customerService=new CustomerService();
 		HttpSession ses=request.getSession();
 		customer =(Customer)ses.getAttribute("CUSTOMERDATA");
 		String excep=(String)ses.getAttribute("ExceptionSend");
-		boolean flag=customerService.printExceptionSend(customer, excep);
+		boolean flag=false;
+		try {
+			flag=customerService.printExceptionSend(customer, excep);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		if(flag)
 		{
 			ses.setAttribute("ExceptionSent", "1");
 			javax.servlet.RequestDispatcher rd = request.getRequestDispatcher("ExceptionPage.jsp");
-			rd.forward(request, response);
+			try {
+				rd.forward(request, response);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
 		}
 		else
 		{
 			ses.setAttribute("ExceptionSent", "0");
 			javax.servlet.RequestDispatcher rd = request.getRequestDispatcher("ExceptionPage.jsp");
-			rd.forward(request, response);
+			try {
+				rd.forward(request, response);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
 		}
-		doGet(request, response);
+		try {
+			doGet(request, response);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	
