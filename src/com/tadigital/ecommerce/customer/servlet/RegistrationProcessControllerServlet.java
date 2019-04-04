511bf503e2d8d116b705ab3718447086ec5758c4
Aditya Sharma <adityasharma_sy@srmuniv.edu.in>
2019-04-05 01:40:47 +0530

    removed warnings and errors

--- src/com/tadigital/ecommerce/customer/servlet/RegistrationProcessControllerServlet.java
+++ src/com/tadigital/ecommerce/customer/servlet/RegistrationProcessControllerServlet.java
@@ -2,8 +2,6 @@ package com.tadigital.ecommerce.customer.servlet;
 
 import java.io.IOException;
 
-
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,61 +15,78 @@ import com.tadigital.ecommerce.customer.service.CustomerService;
 
 @WebServlet("/register")
 public class RegistrationProcessControllerServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		RequestDispatcher rd = req.getRequestDispatcher("SignInSignUpForms.html");
 		rd.forward(req, resp);
 	}
-	
+
+	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//String firstName = req.getParameter("f1");
+
 		String name = req.getParameter("name");
-		//String dateOfJoining = req.getParameter("f3");
 		String email = req.getParameter("email");
 		String password = req.getParameter("pwd");
-		String fName,lName;
-		
-		//System.out.println("Hello World");
+
+		StringBuilder ln = new StringBuilder();
+		String fName;
+		String lName = "";
+		String setLink = "SignInSignUpForms.jsp";
+
 		String str = name;
-        String[] array1 = str.split("\\s+");
-        //System.out.println(""+array1[0]);
-	    fName=array1[0];
-		String ln="";
-		int n=array1.length;
-		//System.out.println(""+n);
-		for(int i=1;i<n;i++)
-		{
-		   ln=ln+" "+array1[i];
+		String[] array1 = str.split("\\s+");
+
+		fName = array1[0];
+
+		int n = array1.length;
+
+		for (int i = 1; i < n; i++) {
+			ln.append(array1[i]);
+			ln.append(" ");
 		}
-		lName=ln;
-		
-		
+		lName = ln.toString();
+
 		Customer customer = new Customer();
 		customer.setfName(fName);
 		customer.setlName(lName);
 		customer.setEmail(email);
 		customer.setPwd(password);
-		
+
+		HttpSession ses = req.getSession();
+
 		CustomerService customerService = new CustomerService();
-		boolean status = customerService.registerCustomer(customer);
-		if(status) {
-			HttpSession ses=req.getSession();
-			ses.setAttribute("check","1");
-			RequestDispatcher rd = req.getRequestDispatcher("SignInSignUpForms.jsp");
-			rd.forward(req, resp);
-			
+		boolean status = false;
+		try {
+			status = customerService.registerCustomer(customer);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (status) {
+
+			ses.setAttribute("check", "1");
+			RequestDispatcher rd = req.getRequestDispatcher(setLink);
+			try {
+				rd.forward(req, resp);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
 		} else {
-			HttpSession ses=req.getSession();
-			ses.setAttribute("check","0");
-			RequestDispatcher rd = req.getRequestDispatcher("SignInSignUpForms.jsp");
-			rd.forward(req, resp);
-			
-				
-			
-			
+
+			ses.setAttribute("check", "0");
+			RequestDispatcher rd = req.getRequestDispatcher(setLink);
+			try {
+				rd.forward(req, resp);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
 		}
 	}
-	
-	
 
 }
