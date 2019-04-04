4740a8d46e5d93e433129e699a0e94fb39780b6f
Aditya Sharma <adityasharma_sy@srmuniv.edu.in>
2019-04-05 01:38:17 +0530

    removed warnings and errors

--- src/com/tadigital/ecommerce/customer/servlet/CustomerAccountDetailsUpdateControllerServlet.java
+++ src/com/tadigital/ecommerce/customer/servlet/CustomerAccountDetailsUpdateControllerServlet.java
@@ -1,9 +1,5 @@
 package com.tadigital.ecommerce.customer.servlet;
 
-
-
-
-
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -17,77 +13,93 @@ import javax.servlet.http.HttpSession;
 import com.tadigital.ecommerce.customer.entity.Customer;
 import com.tadigital.ecommerce.customer.service.CustomerService;
 
-
 @WebServlet("/CustomerAccount")
 public class CustomerAccountDetailsUpdateControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-  
-    public CustomerAccountDetailsUpdateControllerServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	public CustomerAccountDetailsUpdateControllerServlet() {
+		super();
+
+	}
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		Customer customer = new Customer();
-		customer=(Customer)session.getAttribute("CUSTOMERDATA");
-		//String email=customer.getEmail();
-		String password=customer.getPwd();
-		int i=customer.getId();
-		String fname=request.getParameter("f4");
-		String lname=request.getParameter("f5");
-		String gender=request.getParameter("f6");
-		String address=request.getParameter("f7");
+		customer = (Customer) session.getAttribute("CUSTOMERDATA");
+		
+		
+		String fname = request.getParameter("f4");
+		String lname = request.getParameter("f5");
+		String gender = request.getParameter("f6");
+		String address = request.getParameter("f7");
 		String city = request.getParameter("f8");
-		long zip = Long.parseLong(request.getParameter("f9"));
+		try{
+			customer.setZip(Long.parseLong(request.getParameter("f9")));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 		String state = request.getParameter("f10");
 		String country = request.getParameter("f11");
 		String mobile = request.getParameter("f12");
-		String email=request.getParameter("f13");
-		customer.setId(i);
+		String email = request.getParameter("f13");
+		
 		customer.setfName(fname);
 		customer.setlName(lname);
 		customer.setGender(gender);
 		customer.setAddress(address);
 		customer.setCity(city);
-		customer.setZip(zip);
+		
 		customer.setState(state);
 		customer.setCountry(country);
 		customer.setMobile(mobile);
 		customer.setEmail(email);
-		
+
 		CustomerService customerService = new CustomerService();
+		boolean status = false ;
 		
-		boolean status = customerService.updateCustomer(customer);
-		if(status) {
-			System.out.println("Controller"+status);
-			/*HttpSession session = request.getSession();*/
+		try {
+			status = customerService.updateCustomer(customer);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		if (status) {
+			
+			
 			session.setAttribute("CUSTOMERDATA", customer);
 			session.setAttribute("check2", "5");
 			RequestDispatcher rd = request.getRequestDispatcher("CustomerAccount.jsp");
-			System.out.println("Details Updated Succesfull");
-			rd.forward(request, response);
+			
+			try {
+				rd.forward(request, response);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
 		} else {
 			HttpSession ses = request.getSession();
-			session.setAttribute("check2", "-5");
+			ses.setAttribute("check2", "-5");
 			RequestDispatcher rd = request.getRequestDispatcher("CustomerAccount.jsp");
-			System.out.println("Error");
-			//Write Error Message on the screen
-			rd.forward(request, response);
+			
+			try {
+				rd.forward(request, response);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
 		}
-		
-		doGet(request, response);
+
+		try {
+			doGet(request, response);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
-
-
-
