f4ddb669373b63a693125cb4a6081f12f0967821
Aditya Sharma <adityasharma_sy@srmuniv.edu.in>
2019-04-05 01:35:47 +0530

    removed warnings and errors

--- src/com/tadigital/ecommerce/customer/dao/Dao.java
+++ src/com/tadigital/ecommerce/customer/dao/Dao.java
@@ -7,83 +7,64 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 
 public class Dao {
-	
-	//loading the driver and opening a connection
-	protected Connection openConnection()
-	{
-		Connection conn=null;
-		try
-		{
+
+	// loading the driver and opening a connection
+	protected Connection openConnection() {
+		Connection conn = null;
+		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/tp_db","root","");
-		}
-		catch(ClassNotFoundException cnfe) {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tp_db", "root", "");
+		} catch (ClassNotFoundException cnfe) {
 			cnfe.printStackTrace();
-		}
-		catch(SQLException sqle) {
+		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
-		
+
 		return conn;
 	}
-	//creating the statement
-	protected Statement openStatement(Connection conn)
-	{
-		Statement stmt=null;
-		try
-		{
-			stmt=conn.createStatement();
-		}
-		catch(SQLException sqle)
-		{
+
+	// creating the statement
+	protected Statement openStatement(Connection conn) {
+		Statement stmt = null;
+		try {
+			stmt = conn.createStatement();
+		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
 		return stmt;
 	}
-	//closing the result set
-	protected void closeResultSet(ResultSet rs)
-	{
-		try
-		{
-			if(rs!=null)
-			{
+
+	// closing the result set
+	protected void closeResultSet(ResultSet rs) {
+		try {
+			if (rs != null) {
 				rs.close();
 			}
-		}
-		catch(SQLException sqle)
-		{
+		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
 	}
-	//closing the statement
-	protected void closeStatement(Statement stmt)
-	{
-		try
-		{
-			if(stmt!=null)
-			{
+
+	// closing the statement
+	protected void closeStatement(Statement stmt) {
+		try {
+			if (stmt != null) {
 				stmt.close();
 			}
-		}
-		catch(SQLException sqle)
-		{
+		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
 	}
-	//closing the Connection established
-	protected void closeConnection(Connection conn)
-	{
-		try
-		{
-			if(conn!=null)
-			{
+
+	// closing the Connection established
+	protected void closeConnection(Connection conn) {
+		try {
+			if (conn != null) {
 				conn.close();
 			}
-		}
-		catch(SQLException sqle)
-		{
+		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
 	}
-	
+
 }
