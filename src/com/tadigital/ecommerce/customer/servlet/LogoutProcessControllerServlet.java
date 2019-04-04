bb61a063d2a42959f2a5a85a30652c9a36dc6aa8
Aditya Sharma <adityasharma_sy@srmuniv.edu.in>
2019-04-05 01:39:38 +0530

    removed warnings and errors

--- src/com/tadigital/ecommerce/customer/servlet/LogoutProcessControllerServlet.java
+++ src/com/tadigital/ecommerce/customer/servlet/LogoutProcessControllerServlet.java
@@ -1,31 +1,48 @@
 package com.tadigital.ecommerce.customer.servlet;
+
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.tadigital.ecommerce.customer.entity.Customer;
-import com.tadigital.ecommerce.customer.service.CustomerService;
+@WebServlet("/logout")
+public class LogoutProcessControllerServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-
-
-
-	@WebServlet("/logout")
-	public class LogoutProcessControllerServlet extends HttpServlet{
-		@Override
-		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			HttpSession ses=req.getSession();
-			ses.invalidate();
-			RequestDispatcher rd=req.getRequestDispatcher("SignInSignUpForms.jsp");
+		Cookie[] allCookie = req.getCookies();
+		if (allCookie != null) {
+			// SEARCH FOR OUR COOKIE
+			for (Cookie c : allCookie) {
+				String cName = c.getName();
+				if (cName.equals("stay")) {
+					
+					c.setMaxAge(0);
+					resp.addCookie(c);
+					break;
+				}
+			}
+		}
+		HttpSession ses = req.getSession();
+		ses.invalidate();
+		RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
+		try {
 			rd.forward(req, resp);
-		
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
 
 	}
 
+}
